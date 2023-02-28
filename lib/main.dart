ff1f2c1175ff88fb237b578687db49b352dcfc62
NazeerAlFilfil <126560070+NazeerAlFilfil@users.noreply.github.com>
2023-02-28 14:49:40 +0300

    Update main.dart
    Added copyrights

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,9 @@
+/*Copyright (c) 2023, Nazeer Al-Filfil
+All rights reserved.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree. */
+
 import 'package:flutter/material.dart';
 
 void main() {
